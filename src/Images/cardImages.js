a94b3f34852bc35bd2e67ec55e40e7d68d95f179
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-07 18:56:00 -0400

    Make mobile bg pattern preserve aspect ratio.

--- src/Images/cardImages.js
+++ src/Images/cardImages.js
@@ -14,7 +14,7 @@ export const BackgroundPatternDesktop = (props) => {
 
 export const BackgroundPatternMobile = (props) => {
   return (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375 194" preserveAspectRatio="slice" >
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375 194" preserveAspectRatio="xMidYMin slice" >
       <path
         fill="#D6E1FF"
         fillRule="evenodd"
@@ -188,11 +188,11 @@ export const HeroIllustration = (props) => {
 export const MusicIcon = (props) => {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" preserveAspectRatio="slice" >
-      <g fill="none" fill-rule="evenodd">
+      <g fill="none" fillRule="evenodd">
         <circle cx="24" cy="24" r="24" fill="#DFE6FB" />
         <path
           fill="#717FA6"
-          fill-rule="nonzero"
+          fillRule="nonzero"
           d="M32.574 15.198a.81.81 0 00-.646-.19L20.581 16.63a.81.81 0 00-.696.803V26.934a3.232 3.232 0 00-1.632-.44A3.257 3.257 0 0015 29.747 3.257 3.257 0 0018.253 33a3.257 3.257 0 003.253-3.253v-8.37l9.726-1.39v5.327a3.232 3.232 0 00-1.631-.441 3.257 3.257 0 00-3.254 3.253 3.257 3.257 0 003.254 3.253 3.257 3.257 0 003.253-3.253V15.81a.81.81 0 00-.28-.613z"
         />
       </g>
